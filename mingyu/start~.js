2f3319ced87e0969a926508eb2edeb58f228884b
pstar <pstar987@naver.com>
2021-09-25 01:36:08 +0900

    [test]

---
+++ mingyu/start~.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <script>
+            var a = 1; 
+            alert(1+2);
+        </script>
+    </body>
+</html>
