ce6257110aa21bf038605ac458a6e695b21fc4cb
jaehoon0 <choco730522@gmail.com>
2021-10-02 17:57:00 +0900

    create(section2):1주차스터디내용

---
+++ jaehoon/section2/1.dataType/dataType.js
@@ -0,0 +1,14 @@
+//1. Number
+console.log(1+1)
+console.log(4-1)
+console.log(2*2)
+console.log(10/2)
+console.log(10/3)
+console.log(parseInt(10/3))
+
+//2. String
+console.log('1' + '1')
+console.log('hello ' + 'world')
+console.log(1 + '')
+console.log(typeof(1 + ''))
+console.log("fjlwafjelaiwfej".length)
---
+++ jaehoon/section2/2.variable/variable.js
@@ -0,0 +1,11 @@
+var a = 1;
+console.log(a)
+
+let b = 1;
+console.log(b)
+{
+	var c = 3
+	let d = 4
+}
+console.log(c)
+//console.log(d)
